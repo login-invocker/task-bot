8be9884f349afd8b28ed1bbe8f38c65719c5a6a9
tung <dangtung789.td@gmail.com>
2021-02-14 08:50:37 +0700

    update env

--- bot.js
+++ bot.js
@@ -2,6 +2,6 @@ const Discord = require("discord.js")
 require('dotenv')
 const config = require('./config.json');
 let bot = new Discord.Client();
-bot.login(process.env.AUTH_TOKEN || 'ODA2MjE1NTc5NDU0Mjc1NjY2.YBmMxA.NXNp5cQTNqX-16pza5xLJ4aZ3s0');
+bot.login(process.env.AUTH_TOKEN);
 
 module.exports.bot = bot
