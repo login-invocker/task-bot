de357b5d788466c7ec9ba5f2eb0d13bc00d83b17
tung <dangtung789.td@gmail.com>
2021-02-14 09:05:07 +0700

    update

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js")
-require('dotenv')
+require('dotenv').config()
 const config = require('./config.json');
 let bot = new Discord.Client();
 bot.login(process.env.AUTH_TOKEN);
