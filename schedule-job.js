8c69143f9125c63192e21bfe598f2e81352b45d7
Đặng Thanh Tùng <dangtung789.td@gmail.com>
2021-02-15 23:51:05 +0000

    update schedule

--- schedule-job.js
+++ schedule-job.js
@@ -23,8 +23,8 @@ const job = () => {
 const resetTask = (cb) => {
 
   let rule = new schedule.RecurrenceRule();
-  rule.hour = 1;
-  rule.minute = 30;
+  rule.hour = 0;
+  rule.minute = 1;
   rule.tz = 'Asia/Saigon'
 
   const job = schedule.scheduleJob(config.ID_SCHEDULE, rule, ()=>{
